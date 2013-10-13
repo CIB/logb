55f4b571ea6408d8e39bee5588ceab996c1b883f
cib <cib123@googlemail.com>
2013-10-13 15:14:34 +0200

    Reformatted a few files.

--- logb-py/proof_search.py
+++ logb-py/proof_search.py
@@ -1,31 +1,30 @@
 class ProofSearchTree(object):
     """ A tree representing the search for a specific statement. """
-    
+
     def __init__(self, root):
         self.root = ProofSearchTreeNode(root)
-        
+
     def get_root(self):
         return self.root
-        
-        
+
+
 class ProofSearchTreeNode(object):
     def __init__(self, statement):
         self.statement = statement
         self.alternatives = []
-    
+
     def add_alternative(self, statements):
         """ Adds one combination of statements that might yield the desired result. 
             
             @param statements: A set of statements that, if all true(AND) imply self.statement
         
         """
-        
+
         # TODO: check the alternative isn't already there
-        
+
         statement_nodes = []
         for statement in statements:
             statement_nodes.append(ProofSearchTreeNode(statement))
-        
+
         self.alternatives.append(statement_nodes)
-        
-    
+
--- logb-py/rule.py
+++ logb-py/rule.py
@@ -9,26 +9,26 @@ class Rule:
         where %X can be any number of arguments, 
         and A() and B() can be any statement.
     """
-    
+
     def __init__(self):
         self.variables = []
         self.dependencies = []
         self.conclusion = None
-        
+
     def add_variable(self, name):
         rval = pattern.Variable(name)
         self.variables.append(rval)
         return rval
-    
+
     def add_dependency(self, dependency):
         """ Add a new dependency for this rule. """
         self.dependencies.append(dependency)
-        
+
     def set_conclusion(self, conclusion):
         """ Add a new conclusion for this rule. """
         self.conclusion = conclusion
-        
+
     def to_statement(self):
         """ Creates a regular statement, consisting of ForAll and implication, for this rule. """
-        
+
         # TODO
