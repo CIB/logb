bb42dd9a8c02ab626ab06a826c2e8f06e9fd7e37
cib <cib123@googlemail.com>
2014-02-22 20:01:41 +0100

    Added new regression tests for pattern matching.

--- logb-java/src/org/logb/test/RegressionTests.java
+++ logb-java/src/org/logb/test/RegressionTests.java
@@ -1,18 +1,22 @@
 package org.logb.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.logb.core.Entity;
 import org.logb.core.EntityStructure;
+import org.logb.core.EntityStructureBase;
 import org.logb.core.EntityType;
 import org.logb.core.Module;
+import org.logb.core.Pattern;
 import org.logb.core.Statement;
 import org.logb.core.StatementType;
 import org.logb.core.Variable;
@@ -114,6 +118,50 @@ public class RegressionTests {
 		assertNotNull(and2);
 		assertFalse(two.equals(and));
 		assertTrue(and.equals(and2));
+	}
+	
+	@Test
+	public void testMatch() {
+		// Setting up our actual parser.
+		List<StatementType> statementTypes = testModule.getStatementTypes();
+		List<Variable> variables = new ArrayList<Variable>();
+		Variable X = new Variable("X");
+		variables.add(X);
+		EntityParser parser = new EntityParser(testModule.getEntityTypes(), statementTypes, variables);
+		Statement andPattern = (Statement) parser.parse("And(lefthand=X, righthand=X)");
+		Statement andInstance = (Statement) parser.parse("And(lefthand=C, righthand=C)");
+		
+		Pattern pattern = new Pattern(); pattern.setRoot(andPattern);
+		
+		Map<String, EntityStructureBase> substitutions = pattern.match(andInstance);
+		
+		assertNotNull(substitutions);
+		assertNotNull(substitutions.get("X"));
+		assertTrue(substitutions.get("X").equals(new Statement(typeC)));
+	}
+	
+	@Test
+	public void testTwoWayMatch() {
+		// Setting up our actual parser.
+		List<StatementType> statementTypes = testModule.getStatementTypes();
+		List<Variable> variables = new ArrayList<Variable>();
+		Variable X = new Variable("X");
+		variables.add(X);
+		Variable Y = new Variable("Y");
+		variables.add(Y);
+		EntityParser parser = new EntityParser(testModule.getEntityTypes(), statementTypes, variables);
+		Statement andPattern = (Statement) parser.parse("And(lefthand=X, righthand=B)");
+		Statement andInstance = (Statement) parser.parse("And(lefthand=C, righthand=Y)");
+		
+		Pattern pattern = new Pattern(); pattern.setRoot(andPattern);
+		
+		Map<String, EntityStructureBase> substitutions = pattern.match(andInstance);
+		
+		assertNotNull(substitutions);
+		assertNotNull(substitutions.get("X"));
+		assertNotNull(substitutions.get("Y"));
+		assertTrue(substitutions.get("X").equals(new Statement(typeC)));
+		assertTrue(substitutions.get("Y").equals(new Entity(typeB)));
 	}
 	
 }
