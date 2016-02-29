2e0dc66723b5782c3c3539f60570b4f2f1d653f6
Christian Bielert <cib123@googlemail.com>
2016-02-29 16:39:49 +0100

    Fixed pickle.js

--- javascript/pickle.js
+++ javascript/pickle.js
@@ -1,10 +1,10 @@
 exports.types = {};
 exports.serialize = function(instance) {
-    return JSON.stringify(instance);
+    return JSON.stringify(instance, null, 2);
 };
 
 exports.deserialize = function(instance) {
     var rval = JSON.parse(instance);
-    rval.__proto__ = types[rval.type];
+    rval.__proto__ = exports.types[rval.type];
     return rval;
 };
